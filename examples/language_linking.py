678254b52029dce6cd1f9ac9dddd6b8f10c64c86
barabum0 <r.m.poltorabatko@gmail.com>
2023-06-25 20:31:55 +0300

    :sparkles: added example of language linking

---
+++ examples/language_linking.py
@@ -0,0 +1,27 @@
+from aiogram import Dispatcher, Bot, F
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+from aiogram_translation import Translator
+from translations import *
+
+from os import getenv
+
+bot = Bot(getenv("TELEGRAM_TOKEN"))
+dispatcher = Dispatcher()
+translator = Translator()
+
+translator.include([
+    English(),
+    Ukrainian(linked_to_key="ru"),  # Russian will become Ukrainian, instead of default English
+    Ukrainian()
+])
+translator.set_default('en')
+translator.register(dispatcher)
+
+
+@dispatcher.message()
+async def on_message(message: Message, language: BaseTranslation):
+    await message.reply(language.link_lang_message.format(orig_lang=language.linked_to_key, linked_lang=language.key))
+
+
+dispatcher.run_polling(bot)
--- examples/translations.py
+++ examples/translations.py
@@ -6,6 +6,8 @@ class BaseTranslation(BaseTranslationBuilder):
     start_button: str
     start_button_alert: str
 
+    link_lang_message: str
+
 
 class English(BaseTranslation):
     key = "en"
@@ -14,6 +16,8 @@ class English(BaseTranslation):
     start_message = "👋 Hi, I'm bot!"
     start_button = "❤️ Click me!"
     start_button_alert = "🎉 Hooray!"
+
+    link_lang_message = "This must be {orig_lang} language, but it become {linked_lang} language 🤷"
 
 
 class Russian(BaseTranslation):
@@ -24,11 +28,16 @@ class Russian(BaseTranslation):
     start_button = "❤️ Нажми на меня"
     start_button_alert = "🎉 Ура!"
 
+    link_lang_message = "Это должен был быть {orig_lang} язык, но он превратился в {linked_lang} язык 🤷"
+
 
 class Ukrainian(BaseTranslation):
+    # Was translated from Russian with Yandex Translator, so sorry if it got wrong
     key = "uk"
     name = "Український (Ukrainian)"
 
     start_message = "👋 Привіт, я бот"
     start_button = "❤️ Натисни на мене"
     start_button_alert = "🎉 Ура!"
+
+    link_lang_message = "Це мало бути {orig_lang} мова, але вона перетворилася на {linked_lang} мова 🤷"
