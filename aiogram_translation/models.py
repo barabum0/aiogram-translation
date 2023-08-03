0e29bbf054c0bcacd1ab19b2176960f4b69bd174
sushka <barabum@duck.com>
2023-08-03 16:57:36 +0300

    :sparkles: add link_to method

--- aiogram_translation/models.py
+++ aiogram_translation/models.py
@@ -7,3 +7,9 @@ class BaseTranslationBuilder(BaseModel):
     key: str
     linked_to_key: Optional[str] = None
     name: str
+
+    @classmethod
+    def link_to(cls, key: str) -> "BaseTranslationBuilder":
+        return cls(
+            linked_to_key=key
+        )
